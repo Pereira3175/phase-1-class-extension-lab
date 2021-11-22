c432c017d4dffb078928d5338695a81943b94986
Luis <pereira1214@gmail.com>
2021-11-21 23:59:07 -0500

    commit tree b9db3d644579ee398ff2081a1bba065b82ef2fb3

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+class Polygon {
+    constructor (array){
+        this.array = array
+    }
+    get countSides() {
+       return this.array.length
+       console.log(this.countSides)
+    }
+    get perimeter() {
+        let polyPerimeter = this.array
+       
+            return polyPerimeter.reduce((acc, value) => {
+                return acc + value
+            })
+        }
+    }
+
+
+
+class Triangle extends Polygon {
+    get isValid() {
+        let sides = this.array
+        if (sides[0] + sides[1] <= sides[2] || sides[0] + sides[2] <= sides[1] || sides[1] + sides[2] <= sides[0])
+            return false 
+        else
+            return true
+    } 
+
+}
+
+class Square extends Polygon {
+    get area() {
+        let sides = this.array
+        return (sides[0] * sides [0])
+    }
+
+    get isValid() {
+        let sides = this.array
+
+        if (sides[0] == sides[1] && sides[2] && sides[3])
+            return true
+        else 
+            return false
+
+    }
+
+}
